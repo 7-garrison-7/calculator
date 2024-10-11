49a3d79840a7a8dfd6a6a42171aa5edcd1792b0c
Garrison Wright <177910142+7-garrison-7@users.noreply.github.com>
2024-10-11 01:27:25 -0400

    Create number/operator vars and operator fn

--- javascript.js
+++ javascript.js
@@ -13,3 +13,24 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let firstNum;
+let secondNum;
+let operation;
+
+function operate(first, second, operator) {
+    switch (operator) {
+        case "add":
+            return add(first, second);
+            break;
+        case "subtract":
+            return subtract(first, second);
+            break;
+        case "multiply":
+            return multiply(first, second);
+            break;
+        case "divide":
+        return divide(first, second);
+        break;
+    }
+}
